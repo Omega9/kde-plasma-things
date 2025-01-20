4ff132c54b13d47fb297a87d46579a42ddc214bc
Omega9 <nullnine@gmail.com>
2025-01-20 18:49:44 +0500

    + Yt-dlp and Gallery-dl template strings for ShellScript.sh

--- File templates/ShellScript.sh
+++ File templates/ShellScript.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
+gallery-dl --cookies-from-browser vivaldi ''
+torify gallery-dl --cookies-from-browser vivaldi ''
 
+yt-dlp --cookies-from-browser vivaldi ''
+torify yt-dlp --cookies-from-browser vivaldi ''
