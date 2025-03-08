54f692db162d7960c9b6b0e04553a7e38c59a5dc
Omega9 <nullnine@gmail.com>
2025-03-09 03:59:06 +0500

    + yt-dlp --trim-filenames 230

--- File templates/ShellScript.sh
+++ File templates/ShellScript.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
-gallery-dl --cookies-from-browser vivaldi '
-torify gallery-dl --cookies-from-browser vivaldi '
+# gallery-dl --cookies-from-browser vivaldi -P jpg-to-webp '
+# torify gallery-dl --cookies-from-browser vivaldi -P jpg-to-webp '
 
-yt-dlp --cookies-from-browser vivaldi '
-torify yt-dlp --cookies-from-browser vivaldi '
+# gallery-dl --cookies-from-browser vivaldi '
+# torify gallery-dl --cookies-from-browser vivaldi '
+
+# yt-dlp --cookies-from-browser vivaldi --trim-filenames 230 '
+# torify yt-dlp --cookies-from-browser vivaldi --trim-filenames 230 '
