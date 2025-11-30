dfcc266b532afafc53d0db219fcf58b4a138b1bf
Omega9 <nullnine@gmail.com>
2025-12-01 03:59:42 +0500

    + Fixed mkshellscript + Tidy up the actions

--- File templates/ShellScript.sh
+++ File templates/ShellScript.sh
@@ -3,8 +3,8 @@
 # gallery-dl --cookies-from-browser vivaldi -P jpg-to-webp '
 # torify gallery-dl --cookies-from-browser vivaldi -P jpg-to-webp '
 
-# gallery-dl --cookies-from-browser vivaldi '
-# torify gallery-dl --cookies-from-browser vivaldi '
+# yt-dlp --cookies-from-browser vivaldi --trim-filenames 230 -t mp4'
+# torify yt-dlp --cookies-from-browser vivaldi --trim-filenames 230 -t mp4'
 
 # yt-dlp --cookies-from-browser vivaldi --trim-filenames 230 '
 # torify yt-dlp --cookies-from-browser vivaldi --trim-filenames 230 '
