786db75117bfa34632224de082ba3749a6759e68
Omega9 <nullnine@gmail.com>
2026-01-29 01:25:02 +0500

    - Deprecated parameter in ShellScript.sh

--- File templates/ShellScript.sh
+++ File templates/ShellScript.sh
@@ -3,8 +3,5 @@
 # gallery-dl --cookies-from-browser vivaldi -P jpg-to-webp '
 # torify gallery-dl --cookies-from-browser vivaldi -P jpg-to-webp '
 
-# yt-dlp --cookies-from-browser vivaldi --trim-filenames 230 -t mp4'
-# torify yt-dlp --cookies-from-browser vivaldi --trim-filenames 230 -t mp4'
-
 # yt-dlp --cookies-from-browser vivaldi --trim-filenames 230 '
 # torify yt-dlp --cookies-from-browser vivaldi --trim-filenames 230 '
