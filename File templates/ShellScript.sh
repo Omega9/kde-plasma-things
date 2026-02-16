602e7e985577d40bb7edd84fffd7155b888d8635
Omega9 <nullnine@gmail.com>
2026-02-16 15:35:06 +0500

    + `-t mp4` works again + service menu to extract keyfame from static mp4's

--- File templates/ShellScript.sh
+++ File templates/ShellScript.sh
@@ -3,5 +3,5 @@
 # gallery-dl --cookies-from-browser vivaldi -P jpg-to-webp '
 # torify gallery-dl --cookies-from-browser vivaldi -P jpg-to-webp '
 
-# yt-dlp --cookies-from-browser vivaldi --trim-filenames 230 '
-# torify yt-dlp --cookies-from-browser vivaldi --trim-filenames 230 '
+# yt-dlp --cookies-from-browser vivaldi -t mp4 --trim-filenames 230 '
+# torify yt-dlp --cookies-from-browser vivaldi -t mp4 --trim-filenames 230 '
