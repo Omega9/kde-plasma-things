b90b17ebc93b81687e6a247589a42ef3deabb7ab
Omega9 <nullnine@gmail.com>
2026-03-05 01:23:02 +0500

    - ShellScript: torify + ShellScript: DB for yt-dlp and gallery-dl to avoid re-downloading

--- File templates/ShellScript.sh
+++ File templates/ShellScript.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-# gallery-dl --cookies-from-browser vivaldi -P jpg-to-webp '
-# torify gallery-dl --cookies-from-browser vivaldi -P jpg-to-webp '
-
-# yt-dlp --cookies-from-browser vivaldi -t mp4 --trim-filenames 230 '
-# torify yt-dlp --cookies-from-browser vivaldi -t mp4 --trim-filenames 230 '
+# gallery-dl --cookies-from-browser vivaldi --download-archive .gallery-dl.sqlite3 -P jpg-to-webp ''
+# yt-dlp --cookies-from-browser vivaldi --trim-filenames 230 --download-archive .yt-dlp.archive -t mp4 ''
+# yt-dlp --cookies-from-browser vivaldi --trim-filenames 230 --download-archive .yt-dlp.archive ''
